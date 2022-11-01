9b823b73e8a1d8b82521c9d7a1836665e1259baf
kenx00x <kenx00x@gmail.com>
2022-11-01 11:45:16 +0100

    Add project files.

---
+++ code/RaveParty.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+using System;
+
+public class RaveParty
+{
+    static readonly Random Rnd = new();
+
+    [Event.Frame]
+	public static void Rave()
+	{
+        Map.Scene.AmbientLightColor = Color.FromBytes(Rnd.Next(256), Rnd.Next(256), Rnd.Next(256), Rnd.Next(256));
+    }
+}
