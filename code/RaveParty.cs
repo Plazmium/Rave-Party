eb54f045920d9ebbde471134362be8c72f001669
kenx00x <kenx00x@gmail.com>
2022-11-01 12:35:28 +0100

    ravespeed convar

--- code/RaveParty.cs
+++ code/RaveParty.cs
@@ -4,10 +4,17 @@ using System;
 public class RaveParty
 {
     static readonly Random Rnd = new();
+    static float elapsed;
+
+    [ConVar.Replicated("ravespeed", Help = "The time between light changes in seconds (default = 0.1)", Min = 0f, Max = 10f, Saved = true)]
+    public static float ravespeed { get; set; } = 0.1f;
 
     [Event.Frame]
-	public static void Rave()
+    public static void Rave()
 	{
+        elapsed += Time.Delta;
+        if (elapsed < ravespeed) return;
+        elapsed = 0f;
         Map.Scene.AmbientLightColor = Color.FromBytes(Rnd.Next(256), Rnd.Next(256), Rnd.Next(256), Rnd.Next(256));
     }
 }
